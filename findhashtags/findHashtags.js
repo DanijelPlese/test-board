df8f4cdd331f134cbaa2c685f3a849a70d4bbce9
Danijel Pleše <plese.danijel@gmail.com>
2021-02-10 01:18:45 +0100

    setup for hashtags

--- findhashtags/findHashtags.js
+++ findhashtags/findHashtags.js
@@ -2,76 +2,11 @@ const fetch = require("node-fetch");
 const fs = require("fs");
 const { get } = require("http");
 require("dotenv").config();
-const pull = require("./pulls")
+const data = require("./data2");
+
+const z = (async () => {
 
 
-const y = (async () => {
 
-  const urlPull = pull.flat().map(i => {
-    return {
-      contentUrl: i.contentUrl,
-      url: i.url
-    }
-  })
 
-  console.log(urlPull);
-
-  urlPull.forEach(async (i) => {
-    let url = i.contentUrl;
-    let options = {
-      method: "GET",
-      headers: {
-        Accept: "application/vnd.github.v3.raw+json",
-        Authorization: process.env.AUTH,
-      },
-    }
-
-    const body = [];
-
-    const result = await fetch(url, options);
-    //console.log(result)
-    body.push({
-      textBody: await result.text(),
-      url: i.contentUrl,
-    })
-
-    console.log(body)
-
-  })
-
-  
-  //.log(pull.contentUrl)
-
-  /*
-  pull.forEach(async (i) => {
-    let uls = i.contentUrl;
-    let options = {
-      method: "GET",
-      headers: {
-        Accept: "application/vnd.github.v3.raw+json",
-        Authorization: process.env.AUTH,
-      },
-    };
-
-    //console.log(uls)
-    const body = [];
-    //const response = await fetch(uls, options);
-
-    //console.log(response)
-
-    /*
-    body.push({
-      textBody: await responses.text(),
-      url: i.contentUrl,
-    })
-
-    //console.log(body);
-    const matchFor = /([+-]?hashtags: [^\n]*(\n+))/g; //nalazi "+hashtags:" i "-hashtags:" - uz ostatak reda
-
-    //fs.appendFile("./file.json", JSON.stringify(body.flat(), null, 2));
-
-    //console.log(body)
-  });
-
-*/
 })();
