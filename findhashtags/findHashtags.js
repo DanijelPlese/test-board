7403aeb9fe4077ff046e36063dfbdfe2b95a8aa4
Danijel Pleše <plese.danijel@gmail.com>
2021-02-11 02:17:23 +0100

    filtered pulls with changes in hashtags

--- findhashtags/findHashtags.js
+++ findhashtags/findHashtags.js
@@ -1,32 +1,33 @@
-const fetch = require("node-fetch");
 const fs = require("fs");
+const { ContextReplacementPlugin } = require("webpack");
 const data = require("./data1.json"); // [{"textBody", "url"},{...},{...}]
 
-
 const z = (async () => {
-  
-  const allData = data.flat().map((i) => {
-    
-    return {
-      dataPr: i.textBody,
-      url: i.url,
-    };
-  }).filter(t => {
-    const searchFor = /([+-]?hashtags: [^\n]*(\n+))/g;
-    const textBody = t.dataPr;
+  const allDataPR = data
+    .flat()
+    .map((i) => {
+      const searchFor = /([+]hashtags: [^\n]*(\n+))/g;
+      return {
+        hashtags: i.textBody.match(searchFor),
+        url: i.url,
+      };
+    })
+    .filter((i) => i.hashtags !== null)
+    .filter((i) => i.hashtags.length === 2)
+    .map((i) => {
+      return {
+        original: i.hashtags[0],
+        edited: i.hashtags[1],
+        url: i.url,
+      };
+    }); //343 pulls
 
-    const match = textBody.match(searchFor)
-    console.log(match)
+/*
+    allDataPR.forEach(i => {
+      i.original.value.replaceAll(/+hashtags: "/g, '')
+    })
+*/
+  console.log(allDataPR);
 
-
-  });
-
-  //console.log(allData)
-
-
-  const searchFor = /([+-]?hashtags: [^\n]*(\n+))/g;
-
-
-
-
+  //const searchFor = /([+-]?hashtags: [^\n]*(\n+))/g;
 })();
