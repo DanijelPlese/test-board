3ca77c3f96007883648dbf00c931ac86f992d75c
Danijel Pleše <plese.danijel@gmail.com>
2021-02-11 11:52:12 +0100

    extracted added hashtags by pull

--- findhashtags/findHashtags.js
+++ findhashtags/findHashtags.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const { ContextReplacementPlugin } = require("webpack");
 const data = require("./data1.json"); // [{"textBody", "url"},{...},{...}]
 
 const z = (async () => {
@@ -16,15 +15,39 @@ const z = (async () => {
     .filter((i) => i.hashtags.length === 2)
     .map((i) => {
       return {
-        original: i.hashtags[0].replace(/[+]hashtags: "/g, '').replace(/"\n/g, '').split(','),
-        edited: i.hashtags[1].replace(/[+]hashtags: "/g, '').replace(/"\n/g, '').split(','),
+        original: i.hashtags[0]
+          .replace(/[+]hashtags: "/g, "")
+          .replace(/"\n/g, "")
+          .split(",")
+          .sort(),
+        edited: i.hashtags[1]
+          .replace(/[+]hashtags: "/g, "")
+          .replace(/"\n/g, "")
+          .split(",")
+          .sort(),
         url: i.url,
       };
     }); //343 pulls
 
-    
+  const originalTags = allDataPR.map((i) => i.original);
+  const editedTags = allDataPR.map((i) => i.edited);
 
+  allDataPR.forEach((i) => {
+    let oriArr = i.original;
+    let ediArr = i.edited;
+    let url = i.url;
 
-  console.log(originals);
+    const myKeep = ediArr;
+    const toRemove = new Set(oriArr);
 
+    const difference = myKeep.filter((x) => !toRemove.has(x));
+
+    if(difference.length > 0) {
+      console.log(difference)
+    }
+
+    //console.log(difference);
+  });
+
+  //console.log(allDataPR);
 })();
